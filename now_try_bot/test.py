2186cfa2633c62a1a36c704778dfaf5a1ccd4096
PesDavyd <45429036+PesDavyd@users.noreply.github.com>
2020-04-14 04:13:18 +0300

    Delete test.py

--- now_try_bot/test.py
+++
@@ -1,9 +0,0 @@
-import vk_api
-from vk_api.longpoll import VkLongPoll, VkEventType
-
-
-connect = vk_api.VkApi(login='89998403102', password='censured', scope='messages')
-connect.auth()
-
-longpoll = VkLongPoll(connect)
-connect.method('message.send', {'user_id': '222196128', 'message': 'spam'})
